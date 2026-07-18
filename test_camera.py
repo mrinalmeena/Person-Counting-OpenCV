d67ce96f612bbc6ef97cdc1990ccde1b68f6409a
Mrinal Meena <mrinalmeena25@gmail.com>
2026-07-19 00:15:25 +0530

    Update test_camera.py

--- test_camera.py
+++ test_camera.py
@@ -3,14 +3,14 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
-    print("❌ Could not open camera")
+    print(" Could not open camera")
     exit()
 
 while True:
     ret, frame = cap.read()
 
     if not ret:
-        print("❌ Could not read frame")
+        print("Could not read frame")
         break
 
     cv2.imshow("Camera Test", frame)
